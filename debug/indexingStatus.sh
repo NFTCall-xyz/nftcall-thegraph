2b37d87d4e876d6028ec680c6e4dc8cdcca3a0ef
imsunhao <imsunhao@vip.163.com>
2023-03-22 15:31:36 +0800

    feat: add mainnet network

--- debug/indexingStatus.sh
+++ debug/indexingStatus.sh
@@ -1,4 +1,4 @@
 # indexingStatusForPendingVersion
 curl --location --request POST 'https://api.thegraph.com/index-node/graphql' \
   --header 'Content-Type: application/json' \
-  --data-raw '{"query":"{\n  indexingStatusForPendingVersion(subgraphName: \"gordon199404/nftcall\") {\n    synced\n    health\n    entityCount\n    fatalError {\n      message\n      block {\n        number\n        hash\n      }\n      handler\n    }\n    chains {\n      chainHeadBlock {\n        number\n      }\n      latestBlock {\n        number\n      }\n    }\n  }\n}\n","variables":{}}'
+  --data-raw '{"query":"{\n  indexingStatusForPendingVersion(subgraphName: \"nftcall-xyz/nftcall\") {\n    synced\n    health\n    entityCount\n    fatalError {\n      message\n      block {\n        number\n        hash\n      }\n      handler\n    }\n    chains {\n      chainHeadBlock {\n        number\n      }\n      latestBlock {\n        number\n      }\n    }\n  }\n}\n","variables":{}}'
